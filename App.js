ef70ecf5ff5393d87da743a56ab31d7215fb2359
Luis Caro <a1121250026.COMtch.edu.mx>
2022-03-11 18:55:30 -0700

    version 1

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
+import React, {useEffect, useState} from 'react' ;
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
 
 export default function App() {
+const [datos, setData] = useState([]);
+const [isloading, setloading] = useState([]);
+
+    const getPost = async () =>{
+
+      try{
+
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    const response = await fetch(url);
+    const json = await response.json();
+    setData(json);
+  }catch (error){
+    console.error(error);
+  }finally{
+    setloading(false);
+  }
+
+  }
+  useEffect(()=>{
+    getPost();
+  }, []
+
+  )
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {isloading ? 
+      <ActivityIndicator />
+      :(
+       <FlatList data = {datos}
+       keyExtractor = {({id}, index) => id}
+       renderItem = {
+         ({item}) =>(
+           <Text>{item.title}</Text>
+         )
+       }
+       />
+
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
